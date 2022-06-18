6c28a3456aa3291f2711d6f92ac033f5e38975b3
anshika-18 <74226523+anshika-18@users.noreply.github.com>
2022-06-18 14:28:56 +0530

    final changes

--- src/components/singleBlog.js
+++ src/components/singleBlog.js
@@ -1,101 +1,109 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-import Card from "react-bootstrap/Card"
-import {Redirect,Link } from 'react-router-dom'
-import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card";
+import { Redirect, Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 export default class singleBlog extends Component {
+  state = {
+    blog: null,
+    user: null,
+    matched: false,
+  };
 
-    state={
-        blog:null,
-        user:null,
-        matched:false
-    }
+  componentDidMount() {
+    const config = {
+      headers: {
+        "x-auth-token": localStorage.getItem("token"),
+      },
+    };
+    console.log(this.props.match.params.id);
+    const id = this.props.match.params.id;
+    axios
+      .get(`https://blogapp-backend-anshika.herokuapp.com/blog/${id}`, config)
+      .then((blog) => {
+        this.setState({ blog: blog.data });
+      });
+  }
 
-    componentDidMount()
-    {
-        const config={
-            headers:{
-                'x-auth-token':localStorage.getItem('token')
-            }
+  delete = () => {
+    let id = this.props.match.params.id;
+    const config = {
+      headers: {
+        "x-auth-token": localStorage.getItem("token"),
+      },
+    };
+    axios
+      .get(
+        "https://blogapp-backend-anshika.herokuapp.com/api/auth/user",
+        config
+      )
+      .then((user) => {
+        this.setState({ user: user.data });
+        if (this.state.user) {
+          axios
+            .get(`https://blogapp-backend-anshika.herokuapp.com/blog/${id}`)
+            .then((blog) => {
+              this.setState({ blog: blog });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+          if (this.state.user.email === this.state.blog.email) {
+            axios
+              .delete(
+                `https://blogapp-backend-anshika.herokuapp.com/api/blog/${id}`
+              )
+              .then((res) => {
+                console.log(res.data);
+                this.props.history.push("/myblogs");
+              });
+          } else {
+            window.alert(
+              "This blog Does not blog to you..You cannot delete it..!!"
+            );
+          }
+        } else {
+          window.alert("please login to delete");
         }
-        console.log(this.props.match.params.id)
-        const id=this.props.match.params.id
-        axios.get(`https://blogapp-backend-anshika.herokuapp.com/blog/${id}`,config)
-            .then((blog)=>{
-                
-                this.setState({blog:blog.data})
-            })
-    }
+      })
+      .catch((err) => {
+        window.alert("please login to delete");
+      });
+  };
 
-    delete=()=>{
-        let id=this.props.match.params.id
-        const config={
-            headers:{
-                'x-auth-token':localStorage.getItem('token')
-            }
-        }
-        axios.get("https://blogapp-backend-anshika.herokuapp.com/api/auth/user",config)
-            .then(user=>{
-                this.setState({user:user.data})
-                if(this.state.user)
-                {
-                    axios.get(`https://blogapp-backend-anshika.herokuapp.com/blog/${id}`)
-                        .then(blog=>{
-                            this.setState({blog:blog})
-                        })
-                        .catch(err=>{
-                            console.log(err)
-                        })
-                    if(this.state.user.email===this.state.blog.email)
-                    {
-                        axios.delete(`https://blogapp-backend-anshika.herokuapp.com/api/blog/${id}`)
-                            .then(res=>{
-                                console.log(res.data)
-                                this.props.history.push('/myblogs')
-                            })
-                    }
-                    else
-                    {
-                        window.alert("This blog Does not blog to you..You cannot delete it..!!")
-                    }
+  render() {
+    //console.log(this.props)
+    return (
+      <div>
+        {this.state.blog ? (
+          <>
+            <Card className="text-center Card">
+              <Card.Header>{this.state.blog.title}</Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  <b>By: {this.state.blog.author}</b>
+                  <br></br>
+                  {this.state.blog.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
 
-                }
-                else
-                {
-                    window.alert("please login to delete")
-                }
-            })
-            .catch(err=>{
-                window.alert("please login to delete")
-            })
-    }
-
-    render() {
-        //console.log(this.props)
-        return (
-            <div>
-                {this.state.blog
-                ?<>
-                <Card className="text-center Card">
-                        <Card.Header>{this.state.blog.title}</Card.Header>
-                        <Card.Body>
-                            <Card.Text>
-                                <b>By: {this.state.blog.author}</b><br></br>
-                                { this.state.blog.description}
-                            </Card.Text>
-                           
-                        </Card.Body>
-                     </Card>
-                     
-                    <Link to={{pathname:'/update/'+this.state.blog._id}}><Button Variant="primary">Update</Button></Link>
-                    <Button Variant="primary" onClick={this.delete} >Delete</Button>
-                   
-            </>
-            :
-            <></>
-            }
-            </div>
-        )
-    }
+            <Link to={{ pathname: "/update/" + this.state.blog._id }}>
+              <Button Variant="primary">Update</Button>
+            </Link>
+            <Button
+              className="delete-btn"
+              Variant="primary"
+              onClick={this.delete}
+            >
+              Delete
+            </Button>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
 }
